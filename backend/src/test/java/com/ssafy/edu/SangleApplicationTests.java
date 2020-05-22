64b3264f751f3370885302495ef9878af1b96c69
AhyeonKim <kah0396@naver.com>
2020-05-22 17:30:55 +0900

    200522 | AH | backend_base | S02P31B202-31

---
+++ backend/src/test/java/com/ssafy/edu/SangleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
