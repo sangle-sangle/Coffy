14f223aeee076f05881edd543774bbba1465a65d
ssong10 <leesy1403@naver.com>
2020-06-05 14:47:47 +0900

    20200605 | SY | gamecount

--- front/src/api/config/interceptors.js
+++ front/src/api/config/interceptors.js
@@ -4,8 +4,8 @@ import store from '@/store/index.js'
 
 export function setInterceptors() {
   let instance = axios.create({
-    // baseURL: 'http://k02b2021.p.ssafy.io:8197/api/'
-    baseURL : 'http://172.30.1.57:8197/api/',
+    baseURL: 'http://k02b2021.p.ssafy.io:8197/api/'
+    // baseURL : 'http://172.30.1.57:8197/api/',
   })
   instance.interceptors.request.use(
     config => {
